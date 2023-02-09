33df8a09c44825ebb836a9b1266bb35029ebb4de
Yashika-Agrawal <yashikayash2002@gmail.com>
2023-02-10 02:41:54 +0530

    Background Color Switcher
    It's a JavaScript project where on clicking the color buttons the background of the page changes accordingly.

---
+++ script.js
@@ -0,0 +1,21 @@
+var grey=document.getElementById("grey");
+var white=document.getElementById("white");
+var blue=document.getElementById("blue");
+var yellow=document.getElementById("yellow");
+grey.addEventListener("click",function(){
+    console.log('grey');
+    document.body.style.backgroundColor="grey";
+});
+white.addEventListener("click",function(){
+    console.log('grey');
+    document.body.style.backgroundColor="white";
+});
+blue.addEventListener("click",function(){
+    console.log('grey');
+    document.body.style.backgroundColor="blue";
+});
+yellow.addEventListener("click",function(){
+    console.log('grey');
+    document.body.style.backgroundColor="yellow";
+});
+
